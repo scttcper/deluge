511e110d3e7290db17944b4f7ee1dfabc441ebef
Scott Cooper <scttcper@gmail.com>
2022-06-02 22:01:20 -0700

    fix: upgrade dependencies

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,19 +2,16 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-import test from 'ava';
 import pWaitFor from 'p-wait-for';
+import { afterEach, beforeAll, expect, it } from 'vitest';
 
 import { Deluge } from '../src/index.js';
 import { TorrentListResponse } from '../src/types.js';
 
 const baseUrl = 'http://localhost:8112';
 const torrentName = 'ubuntu-18.04.1-desktop-amd64.iso';
-const __dirname = new URL('.', import.meta.url).pathname;
 const torrentFile = path.join(__dirname, '/ubuntu-18.04.1-desktop-amd64.iso.torrent');
 const torrentHash = 'e84213a794f3ccd890382a54a64ca68b7e925433';
-const magnet =
-  'magnet:?xt=urn:btih:B0B81206633C42874173D22E564D293DAEFC45E2&dn=Ubuntu+11+10+Alternate+Amd64+Iso&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.open-internet.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.si%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Fdenis.stalker.upeer.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce';
 
 async function setupTorrent(deluge: Deluge): Promise<TorrentListResponse> {
   await deluge.addTorrent(torrentFile, { add_paused: true });
@@ -26,15 +23,15 @@ async function setupTorrent(deluge: Deluge): Promise<TorrentListResponse> {
     { timeout: 10000 },
   );
   const res = await deluge.listTorrents();
-  // expect(Object.keys(res.result.torrents)).toHaveLength(1);
+  expect(Object.keys(res.result.torrents)).toHaveLength(1);
   return res;
 }
 
-test.serial.before(async () => {
+beforeAll(async () => {
   const deluge = new Deluge({ baseUrl });
   await deluge.enablePlugin(['Label']);
 });
-test.serial.afterEach(async () => {
+afterEach(async () => {
   const deluge = new Deluge({ baseUrl });
   const torrents = await deluge.listTorrents();
   const ids = Object.keys(torrents.result.torrents);
@@ -43,39 +40,39 @@ test.serial.afterEach(async () => {
     await deluge.removeTorrent(id, true);
   }
 });
-test.serial('should be instantiable', (t) => {
+it('should be instantiable', () => {
   const deluge = new Deluge({ baseUrl });
-  t.truthy(deluge);
+  expect(deluge).toBeTruthy();
 });
-test.serial('should disconnect', async (t) => {
+it('should disconnect', async () => {
   const deluge = new Deluge({ baseUrl });
   await deluge.connect();
   const res = await deluge.disconnect();
-  t.true(res);
+  expect(res).toBe(true);
 });
-test.serial('should connect', async (t) => {
+it('should connect', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.connect();
   // theres a bunch
-  t.assert(res.result.length > 2);
+  expect(res.result.length).toBeGreaterThan(2);
 });
-test.serial('should get plugins', async (t) => {
+it('should get plugins', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.getPlugins();
-  t.assert(res.result.enabled_plugins.length > 0);
-  t.truthy(res.result.available_plugins);
-  t.assert(res.result.available_plugins.includes('Label'));
+  expect(res.result.enabled_plugins.length).toBeGreaterThan(0);
+  expect(res.result.available_plugins).toBeDefined();
+  expect(res.result.available_plugins).toContain('Label');
 });
-test.serial('should get plugins info', async (t) => {
+it('should get plugins info', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.getPluginInfo(['Label']);
-  t.is(res.result.License, 'GPLv3');
+  expect(res.result.License).toBe('GPLv3');
 });
-test.serial('should get version', async (t) => {
+it('should get version', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.getVersion();
   console.log({ version: res.result });
-  t.true(res.result.startsWith('2.0'));
+  expect(res.result.startsWith('2.0')).toBe(true);
 });
 // for some reason explodes deluge
 // it('should enable/disable plugins', async () => {
@@ -85,201 +82,200 @@ test.serial('should get version', async (t) => {
 //   expect(after.result.enabled_plugins).toEqual(['Label']);
 //   await deluge.disablePlugin(['Label']);
 // });
-test.serial('should get config', async (t) => {
+it('should get config', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.getConfig();
-  t.truthy(res.result.dht);
+  expect(res.result.dht).toBeDefined();
 });
-test.serial('should set config', async (t) => {
+it('should set config', async () => {
   const deluge = new Deluge({ baseUrl });
   const startConfig = await deluge.getConfig();
-  t.true(startConfig.result.upnp);
+  expect(startConfig.result.upnp).toBe(true);
   await deluge.setConfig({ upnp: false });
   const res = await deluge.getConfig();
-  t.false(res.result.upnp);
+  expect(res.result.upnp).toBe(false);
   await deluge.setConfig({ upnp: true });
 });
-test.serial('should login', async (t) => {
+it('should login', async () => {
   const deluge = new Deluge({ baseUrl });
   const success = await deluge.login();
-  t.true(success);
+  expect(success).toBe(true);
 });
-test.serial('should logout', async (t) => {
+it('should logout', async () => {
   const deluge = new Deluge({ baseUrl });
   await deluge.login();
   const success = await deluge.logout();
-  t.true(success);
+  expect(success).toBe(true);
 });
-test.serial('should change password', async (t) => {
+it('should change password', async () => {
   const deluge = new Deluge({ baseUrl });
   const oldPassword = 'deluge';
   const newPassword = 'deluge1';
   // change password
-  t.is(deluge.config.password, oldPassword);
+  expect(deluge.config.password).toBe(oldPassword);
   const res = await deluge.changePassword(newPassword);
-  t.is(res.result, true);
-  t.is(deluge.config.password, newPassword);
+  expect(res.result).toBe(true);
+  expect(deluge.config.password).toBe(newPassword);
   // change password back
   const res1 = await deluge.changePassword(oldPassword);
-  t.true(res1.result);
-  t.is(deluge.config.password, oldPassword);
+  expect(res1.result).toBe(true);
+  expect(deluge.config.password).toBe(oldPassword);
   deluge.config.password = 'wrongpassword';
-  await t.throwsAsync(deluge.changePassword('shouldfail'));
+  await expect(deluge.changePassword('shouldfail')).rejects.toThrowError();
 });
-test.serial('should list methods', async (t) => {
+it('should list methods', async () => {
   const deluge = new Deluge({ baseUrl });
   const methods = await deluge.listMethods();
-  t.true(Array.isArray(methods.result));
-  t.assert(methods.result.length >= 88);
+  expect(Array.isArray(methods.result)).toEqual(true);
+  expect(methods.result.length).toBeGreaterThanOrEqual(88);
 });
-test.serial('should upload torrent from full path', async (t) => {
+it('should upload torrent from full path', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.upload(torrentFile);
-  t.assert(res.files.length === 1);
-  t.true(res.success);
+  expect(res.files.length).toBe(1);
+  expect(res.success).toBe(true);
 });
-test.serial('should add torrent from file path string', async (t) => {
+it('should add torrent from file path string', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.addTorrent(torrentFile);
-  t.true(res.result[0][0]);
-  t.is(res.result[0][1], torrentHash);
+  expect(res.result[0][0]).toBe(true);
+  expect(res.result[0][1]).toBe(torrentHash);
 });
-test.serial('should add torrent from file buffer', async (t) => {
+it('should add torrent from file buffer', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await deluge.addTorrent(fs.readFileSync(torrentFile));
-  t.true(res.result[0][0]);
-  t.is(res.result[0][1], torrentHash);
+  expect(res.result[0][0]).toBe(true);
+  expect(res.result[0][1]).toBe(torrentHash);
 });
-test.serial('should add torrent from file contents base64', async (t) => {
+it('should add torrent from file contents base64', async () => {
   const deluge = new Deluge({ baseUrl });
   const contents = Buffer.from(fs.readFileSync(torrentFile)).toString('base64');
   const res = await deluge.addTorrent(contents);
-  t.true(res.result[0][0]);
-  t.is(res.result[0][1], torrentHash);
+  expect(res.result[0][0]).toBe(true);
+  expect(res.result[0][1]).toBe(torrentHash);
 });
-test.serial('should get torrent status', async (t) => {
+it('should get torrent status', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await setupTorrent(deluge);
   const keys = Object.keys(res.result.torrents);
   for (const key of keys) {
     const status = await deluge.getTorrentStatus(key);
-    t.is(status.result.name, torrentName);
+    expect(status.result.name).toEqual(torrentName);
   }
 });
-test.serial('should list torrents', async (t) => {
+it('should list torrents', async () => {
   const deluge = new Deluge({ baseUrl });
   await setupTorrent(deluge);
   const res = await deluge.listTorrents();
-  t.truthy(res.result.torrents);
+  expect(res.result.torrents).toBeDefined();
   const keys = Object.keys(res.result.torrents);
-  t.is(keys.length, 1);
+  expect(keys.length).toEqual(1);
   for (const key of keys) {
     const torrent = res.result.torrents[key];
-    t.true(torrent.is_auto_managed);
+    expect(torrent.is_auto_managed).toBe(true);
   }
 });
-test.serial('should get array of normalized torrent data', async (t) => {
+it('should get array of normalized torrent data', async () => {
   const deluge = new Deluge({ baseUrl });
   await setupTorrent(deluge);
   const res = await deluge.getAllData();
-  t.is(res.torrents.length, 1);
+  expect(res.torrents).toHaveLength(1);
   for (const torrent of res.torrents) {
-    t.truthy(torrent.id);
-    t.is(torrent.name, torrentName);
+    expect(torrent.id).toBeDefined();
+    expect(torrent.name).toBe(torrentName);
   }
 });
-test.serial('should get normalized torrent data', async (t) => {
+it('should get normalized torrent data', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await setupTorrent(deluge);
   const keys = Object.keys(res.result.torrents);
   for (const key of keys) {
     const torrent = await deluge.getTorrent(key);
-    t.is(torrent.name, torrentName);
+    expect(torrent.name).toEqual(torrentName);
   }
 });
-test.serial('should move torrents in queue', async (t) => {
+it('should move torrents in queue', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await setupTorrent(deluge);
   const key = Object.keys(res.result.torrents)[0];
-  t.truthy(await deluge.queueUp(key));
-  t.truthy(await deluge.queueDown(key));
-  t.truthy(await deluge.queueTop(key));
-  t.truthy(await deluge.queueBottom(key));
+  await deluge.queueUp(key);
+  await deluge.queueDown(key);
+  await deluge.queueTop(key);
+  await deluge.queueBottom(key);
 });
-test.serial('should force recheck torrent', async (t) => {
+it('should force recheck torrent', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await setupTorrent(deluge);
   const key = Object.keys(res.result.torrents)[0];
-  t.truthy(await deluge.verifyTorrent(key));
+  await deluge.verifyTorrent(key);
 });
-test.serial('should update torrent trackers', async (t) => {
+it('should update torrent trackers', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await setupTorrent(deluge);
   const key = Object.keys(res.result.torrents)[0];
-  t.truthy(await deluge.updateTorrentTrackers(key));
+  await deluge.updateTorrentTrackers(key);
 });
-test.serial('should add label', async (t) => {
+it('should add label', async () => {
   const client = new Deluge({ baseUrl });
   const list = await setupTorrent(client);
   const key = Object.keys(list.result.torrents)[0];
   await client.addLabel('swag');
   const res = await client.setTorrentLabel(key, 'swag');
   await client.removeLabel('swag');
-  t.is(res.result, null);
+  expect(res.result).toBe(null);
 });
-test.serial('should pause/resume torrents', async (t) => {
+it('should pause/resume torrents', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await setupTorrent(deluge);
   const keys = Object.keys(res.result.torrents);
   for (const key of keys) {
-    t.truthy(await deluge.pauseTorrent(key));
+    await deluge.pauseTorrent(key);
   }
 
   for (const key of keys) {
-    t.truthy(await deluge.resumeTorrent(key));
+    await deluge.resumeTorrent(key);
   }
 });
-test.serial('should set torrent options', async (t) => {
+it('should set torrent options', async () => {
   const deluge = new Deluge({ baseUrl });
   const res = await setupTorrent(deluge);
   const keys = Object.keys(res.result.torrents);
   for (const key of keys) {
-    t.truthy(await deluge.setTorrentOptions(key, { max_download_speed: 22 }));
-    t.truthy(await deluge.setTorrentOptions(key, { max_download_speed: 0 }));
+    await deluge.setTorrentOptions(key, { max_download_speed: 22 });
+    await deluge.setTorrentOptions(key, { max_download_speed: 0 });
   }
 });
-test.serial('should error when torrent hash does not exist', async (t) => {
+it('should error when torrent hash does not exist', async () => {
   const client = new Deluge({ baseUrl });
-  await t.throwsAsync(client.getTorrentStatus('abc123hash'));
+  await expect(client.getTorrentStatus('abc123hash')).rejects.toThrowError();
 });
-test.serial('should return normalized torrent data', async (t) => {
+it('should return normalized torrent data', async () => {
   const client = new Deluge({ baseUrl });
   await setupTorrent(client);
   const res = await client.getAllData();
   const torrent = res.torrents[0];
-  t.is(torrent.connectedPeers, 0);
-  t.is(torrent.connectedSeeds, 0);
-  t.is(torrent.downloadSpeed, 0);
-  t.is(torrent.eta, 0);
+  expect(torrent.connectedPeers).toBe(0);
+  expect(torrent.connectedSeeds).toBe(0);
+  expect(torrent.downloadSpeed).toBe(0);
+  expect(torrent.eta).toBe(0);
   // expect(torrent.isCompleted).toBe(false);
   // expect(torrent.label).toBe(undefined);
-  t.is(torrent.name, torrentName);
-  t.assert(torrent.progress >= 0);
-  t.is(torrent.queuePosition, 1);
+  expect(torrent.name).toBe(torrentName);
+  expect(torrent.progress).toBeGreaterThanOrEqual(0);
+  expect(torrent.queuePosition).toBe(1);
   // expect(torrent.ratio).toBe(-1);
   // expect(torrent.savePath).toBe('/root/Downloads');
   // expect(torrent.state).toBe('checking');
   // expect(torrent.stateMessage).toBe('');
-  t.is(torrent.totalDownloaded, 0);
-  t.is(torrent.totalPeers, -1);
-  t.is(torrent.totalSeeds, -1);
-  t.is(torrent.totalSelected, 1953349632);
+  expect(torrent.totalDownloaded).toBe(0);
+  expect(torrent.totalPeers).toBe(-1);
+  expect(torrent.totalSeeds).toBe(-1);
+  expect(torrent.totalSelected).toBe(1953349632);
   // expect(torrent.totalSize).toBe(undefined);
-  t.is(torrent.totalUploaded, 0);
-  t.is(torrent.uploadSpeed, 0);
+  expect(torrent.totalUploaded).toBe(0);
+  expect(torrent.uploadSpeed).toBe(0);
 });
-test.serial('should add torrent with normalized response', async (t) => {
-  t.timeout(15000);
+it('should add torrent with normalized response', async () => {
   const client = new Deluge({ baseUrl });
 
   // try adding label
@@ -290,40 +286,34 @@ test.serial('should add torrent with normalized response', async (t) => {
   const torrent = await client.normalizedAddTorrent(fs.readFileSync(torrentFile), {
     label: 'test',
   });
-  t.assert(torrent.connectedPeers >= 0);
-  t.assert(torrent.connectedSeeds >= 0);
-  t.assert(torrent.downloadSpeed >= 0);
-  t.assert(torrent.eta >= 0);
+  expect(torrent.connectedPeers).toBeGreaterThanOrEqual(0);
+  expect(torrent.connectedSeeds).toBeGreaterThanOrEqual(0);
+  expect(torrent.downloadSpeed).toBeGreaterThanOrEqual(0);
+  expect(torrent.eta).toBeGreaterThanOrEqual(0);
   // expect(torrent.isCompleted).toBe(false);
   // its setting the label but it takes an unknown number of seconds to save to db
   // expect(torrent.label).toBe('');
-  t.is(torrent.name, torrentName);
-  t.assert(torrent.progress >= 0);
-  t.is(torrent.queuePosition, 1);
+  expect(torrent.name).toBe(torrentName);
+  expect(torrent.progress).toBeGreaterThanOrEqual(0);
+  expect(torrent.queuePosition).toBe(1);
   // expect(torrent.ratio).toBe(-1);
   // expect(torrent.savePath).toBe('/downloads/');
   // expect(torrent.state).toBe(TorrentState.checking);
   // expect(torrent.stateMessage).toBe('');
-  t.assert(torrent.totalDownloaded >= 0);
-  t.is(torrent.totalPeers, -1);
-  t.is(torrent.totalSeeds, -1);
-  t.is(torrent.totalSelected, 1953349632);
+  expect(torrent.totalDownloaded).toBeGreaterThanOrEqual(0);
+  expect(torrent.totalPeers).toBe(-1);
+  expect(torrent.totalSeeds).toBe(-1);
+  expect(torrent.totalSelected).toBe(1953349632);
   // expect(torrent.totalSize).toBe(undefined);
-  t.assert(torrent.totalUploaded >= 0);
-  t.assert(torrent.uploadSpeed >= 0);
-});
-test.serial('should add normalized torrent from magnet', async (t) => {
-  const client = new Deluge({ baseUrl });
-  const torrent = await client.normalizedAddTorrent(magnet);
-  t.is(torrent.name, 'Ubuntu 11 10 Alternate Amd64 Iso');
-});
-test.serial.skip('should download from url', async (t) => {
-  t.timeout(15000);
+  expect(torrent.totalUploaded).toBeGreaterThanOrEqual(0);
+  expect(torrent.uploadSpeed).toBeGreaterThanOrEqual(0);
+}, 15000);
+it('should download from url', async () => {
   const client = new Deluge({ baseUrl });
   const result = await client.downloadFromUrl(
     'https://releases.ubuntu.com/20.10/ubuntu-20.10-desktop-amd64.iso.torrent',
   );
-  t.assert(result.includes('/tmp/'));
+  expect(result).toContain('/tmp/');
   await client.addTorrent(result, { add_paused: true });
   await pWaitFor(
     async () => {
@@ -333,5 +323,5 @@ test.serial.skip('should download from url', async (t) => {
     { timeout: 10000 },
   );
   const res = await client.listTorrents();
-  t.is(Object.keys(res.result.torrents).length, 1);
-});
+  expect(Object.keys(res.result.torrents)).toHaveLength(1);
+}, 15000);
