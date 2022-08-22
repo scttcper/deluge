866b103e46e1e6b5c16320a811b7c8ca9e4dfc6e
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-08-23 01:13:38 +0200

    fix: update incorrect GetHostStatusResponse and GetHostsResponse (#108)

--- src/types.ts
+++ src/types.ts
@@ -39,7 +39,7 @@ export interface GetHostsResponse extends DefaultResponse {
   /**
    * host id - ddf084f5f3d7945597991008949ea7b51e6b3d93
    * ip address - 127.0.0.1
-   * not sure? - 58846
+   * port - 58846
    * status - "Online"
    */
   result: Array<[string, string, number, string]>;
@@ -49,12 +49,10 @@ export type HostStatus = 'Online' | 'Offline' | 'Connected';
 export interface GetHostStatusResponse extends DefaultResponse {
   /**
    * host id - ddf084f5f3d7945597991008949ea7b51e6b3d93
-   * ip address - 127.0.0.1
-   * not sure? - 58846
    * status - "Online"
    * version - "1.3.15"
    */
-  result: [string, string, number, HostStatus, string];
+  result: [string, HostStatus, string];
 }
 
 export interface TorrentContentFile {
