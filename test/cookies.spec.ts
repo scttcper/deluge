f36549110363255631fcbc819c8a2ea41d4d1b5b
Scott Cooper <scttcper@gmail.com>
2026-03-23 21:25:51 -0700

    lint

--- test/cookies.spec.ts
+++ test/cookies.spec.ts
@@ -6,14 +6,18 @@ const baseUrl = 'http://localhost:8112';
 
 it('should build a request cookie header from the auth set-cookie header', () => {
   const deluge = new Deluge({ baseUrl });
-  (deluge as any)._setAuthCookie('session_id=abc123; expires=Wed, 09 Jun 2027 10:18:14 GMT; HttpOnly');
+  (deluge as any)._setAuthCookie(
+    'session_id=abc123; expires=Wed, 09 Jun 2027 10:18:14 GMT; HttpOnly',
+  );
 
   expect(deluge.state.auth.cookieHeader).toBe('session_id=abc123');
 });
 
 it('should preserve the auth cookie expiration from set-cookie', () => {
   const deluge = new Deluge({ baseUrl });
-  (deluge as any)._setAuthCookie('session_id=abc123; expires=Wed, 09 Jun 2027 10:18:14 GMT; HttpOnly');
+  (deluge as any)._setAuthCookie(
+    'session_id=abc123; expires=Wed, 09 Jun 2027 10:18:14 GMT; HttpOnly',
+  );
 
   expect(deluge.state.auth.expires).toBe('2027-06-09T10:18:14.000Z');
 });
